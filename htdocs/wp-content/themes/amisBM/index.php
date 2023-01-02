fde080b940faa62ef1c6548a4cd91255450074fa
Arthur CLOUX <arthur.cloux@gmail.com>
2023-01-02 10:50:09 +0100

    style: add styles to home page

--- htdocs/wp-content/themes/amisBM/index.php
+++ htdocs/wp-content/themes/amisBM/index.php
@@ -40,6 +40,31 @@ get_header();
             </div>
         </div>
     </section>
+
+    <section class="home_publications">
+        <div class="publications--bgdImg" style="">
+            <img src="<?php echo get_template_directory_uri() . "/assets/images/home-publication.jpg"; ?>" alt='Image de fond de la section "les publications"'>
+        </div>
+
+        <div class="publications--content section_padding">
+            <h2 class="section_title section_title--light">Les Publications</h2>
+    
+            <div class="publications_txtWrapper">
+                <p>Depuis bientôt 70 ans, l'association contribue à la découverte de l'histoire de Bourron-Marlotte, de ses habitants, à la mise en valeur des sites et des monuments, à l'animation culturelle et à la promotion du village.</p>
+                <p>Nous accueillons tous les bénévoles passionnés et amoureux de Bourron-Marlotte, avec qui nous pourrons poursuivre la découverte des riches éléments patrimoniaux qui nous entourent.</p>
+            </div>
+    
+            <button class="mainBtn">Voir toutes les publications</button>
+        </div>
+    </section>
+
+    <section class="home_join section_padding">
+        <h2 class="join_txt">Envie de vous impliquer ou simplement envie de devenir un ami de Bourron et de soutenir nos activités ?</h2>
+
+        <div class="join_btnWrapper">
+            <button class="mainBtn">Rejoindre l'association</button>
+        </div>
+    </section>
 </main>
 
 <?php
