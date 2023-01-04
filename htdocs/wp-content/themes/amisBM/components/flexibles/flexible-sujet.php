84c2803f69e222f9b57d71b57a8050994cb3b25e
thomas <hello@thomasflorentin.net>
2023-01-04 14:39:46 +0100

    feat: Comment

--- htdocs/wp-content/themes/amisBM/components/flexibles/flexible-sujet.php
+++ htdocs/wp-content/themes/amisBM/components/flexibles/flexible-sujet.php
@@ -6,6 +6,7 @@
                             <img src="http://amisbm/wp-content/uploads/2022/12/sujetdumois.png" alt="">
                         </div>
 
+                        <!-- ICI, mettre un Champ ACF OBJET POST-->
                         <div class="sujetDuMois_txt">
                             <h3>Un peintre guadeloupéen à Bourron-Marlotte :</h3>
                             <p>Après avoir produit plusieurs décorations murales dans divers édifices de Guadeloupe et de Martinique, puis publié l'ouvrage de référence La Guadeloupe Pittoresque, richement illustré par ses soins, Armand Budan (1827-1874) tente sa chance à Paris au début des années 1860. La renommée des artistes bellifontains l'attire à Bourron-Marlotte, où il passera plusieurs années avec sa famille au Pavé du Roy. Peu de ses peintures nous sont parvenues. Budan a ensuite ouvert un atelier de photographie à Paris, avant de finir ses jours en Guadeloupe.</p>
