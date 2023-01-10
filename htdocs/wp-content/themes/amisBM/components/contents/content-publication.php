544be12ae2dd507d696c4fbc463692db65ffac4c
thomas <hello@thomasflorentin.net>
2023-01-10 17:01:25 +0100

    single: Publication

---
+++ htdocs/wp-content/themes/amisBM/components/contents/content-publication.php
@@ -0,0 +1,55 @@
+<article id="post-<?php the_ID() ?>">
+
+    <section>
+        <?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
+
+            <div class="post_otherInfos">
+                <span><?php the_field('publication_date'); ?></span>
+                <div class="separator--dot"></div>
+                <?php
+
+                    $term_obj_list = get_the_terms( $post->ID, 'publication_type' );
+                    $terms_string = join(', ', wp_list_pluck($term_obj_list, 'name'));
+                    echo $terms_string;
+
+                ?>
+            </div>
+
+            <?php
+                $tags = wp_get_post_tags($post->ID); 
+                $html = '<ul class="publication_tags">';
+                foreach ( $tags as $tag ) {
+                    $html .= "<li>";
+                        $html .= "<span>{$tag->name}</span>";
+                        $html .=  '<span class="separator--dot"></span>';
+                    $html .= "</li>";
+                }
+                $html .= '</ul>';
+                echo $html; ?>
+
+    </section>
+
+
+    <section class="publication-content">
+
+        <?php
+            if(has_post_thumbnail()):
+                echo '<div class="content_media">';
+                    the_post_thumbnail('large');
+                echo '</div>'; 
+            endif;
+        ?>
+
+        <div class="content_text">
+            <?php the_content(); ?>
+
+            <?php the_content(); ?>
+        </div>
+
+    </section>
+
+    <section class="publication-action">
+        <a href="#" class="mainBtn">Commander</a>
+    </section>
+
+</article>
--- htdocs/wp-content/themes/amisBM/single-publication.php
+++ htdocs/wp-content/themes/amisBM/single-publication.php
@@ -2,20 +2,11 @@
     get_header();
 ?>
 
-<section class="home_hero">
-    <div class="hero--img">
-        <img src="<?php echo get_template_directory_uri() . "/assets/images/home-hero.png"; ?>" alt="Image d'introduction de la page d'accueil">
-    </div>
-    <div class="hero--content">
-        <?php the_title( '<h1 class="single-title">', '</h1>' ); ?>
-    </div>
-</section>
-
 <main class="main content_area section_padding">
     <?php
         while(have_posts()):
             the_post();
-            get_template_part('components/contents/content', 'post');
+            get_template_part('components/contents/content', 'publication');
 
         endwhile;
     ?>
