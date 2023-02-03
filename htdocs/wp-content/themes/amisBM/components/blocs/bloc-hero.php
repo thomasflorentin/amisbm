07fafe46ad1dec7f6551ee8f1d329d70702077af
thomas <hello@thomasflorentin.net>
2023-02-03 11:48:17 +0100

    fix: Hero timer

--- htdocs/wp-content/themes/amisBM/components/blocs/bloc-hero.php
+++ htdocs/wp-content/themes/amisBM/components/blocs/bloc-hero.php
@@ -3,7 +3,7 @@
 
 
 
-<section data-timer="<?php the_field('hero_timer'); ?>" class="<?php echo is_front_page() ? 'home_hero' : 'post_hero'; ?>">
+<section data-timer="<?php the_field('hero_timer', get_option('page_on_front')); ?>" class="<?php echo is_front_page() ? 'home_hero' : 'post_hero'; ?>">
     <??>
     <div class="hero--img">
         <?php 
