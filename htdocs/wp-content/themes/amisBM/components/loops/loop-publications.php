f9b71b8134add3f671a274e401020573922c43d7
thomas <hello@thomasflorentin.net>
2023-02-13 08:59:05 +0100

    style: Publications grid

--- htdocs/wp-content/themes/amisBM/components/loops/loop-publications.php
+++ htdocs/wp-content/themes/amisBM/components/loops/loop-publications.php
@@ -3,7 +3,7 @@
 
         while ( $the_query->have_posts() ) {
             $the_query->the_post();
-            echo '<div class="s_6col m_3col l_2col">';
+            echo '<div class="s_3col m_2col l_2col">';
                 get_template_part('components/blocs/bloc', 'publication');
             echo '</div>';
         }
