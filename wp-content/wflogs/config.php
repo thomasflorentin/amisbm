1ab205a30e2f83cba0de14a0923d32f9e4bc6056
Thomas <hello@thomasflorentin.net>
2026-05-11 09:31:09 +0200

    remove from git

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1733240408;s:7:"authKey";s:64:"4T2$DSRMwe]ik$2Nx8R@DrNPD~~G,0q4S^f[TJiX]2-g82 #gG`0nCP^8)v:t~60";s:7:"version";s:5:"1.1.0";}
